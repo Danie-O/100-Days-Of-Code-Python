c310037a455eb9cf71701eb9e60f3f9950342d9f
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-05-05 01:38:05 +0000

    Practice with more examples

--- Day1/print-debugging-practice.py
+++ Day1/print-debugging-practice.py
@@ -11,3 +11,21 @@ print("Day 1 - String Manipulation")
 print("String Concatenation is done with the \"+\" sign.")
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+
+# Print user specific welcome message
+name = input("What is your name: ")
+print("Hello " + name + ", hope you've been well!")
+
+
+# Switch and return user-supplied values
+a = input("a: ")
+b = input("b: ")
+a, b = b, a
+
+print("a: " + a)
+print("b: " + b)
+
+
+
+
+
