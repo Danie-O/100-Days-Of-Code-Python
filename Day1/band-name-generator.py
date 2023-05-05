75cf78fa5186cc103ffea907f697ba8d3a811fe9
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-05-05 01:46:38 +0000

    Add file via upload

---
+++ Day1/band-name-generator.py
@@ -0,0 +1,8 @@
+# Band name generator using city and pet name entered by user
+
+print("Hello, welcome to your favorite band name generator!")
+# request for input
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your favorite pet?\n")
+print("Your bandname is " + city + " " + pet)
+
